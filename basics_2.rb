bff7ec9d60fa440e8902ad346efdeb8f8b45a3cf
Radu Constantin <constantin.raduadrian@yahoo.com>
2019-08-07 19:39:34 +0300

    Updated to show all digits.

--- basics_2.rb
+++ basics_2.rb
@@ -1,4 +1,10 @@
 puts "Insert number:"
 number = gets.to_i
-result = ("number"/100)%10
-puts result
+first_digit = number / 1000
+second_digit = (number / 100) % 10
+third_digit = (number / 10) % 10
+fourth_digit = number % 10
+puts first_digit
+puts second_digit
+puts third_digit
+puts fourth_digit
