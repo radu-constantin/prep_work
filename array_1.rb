b647f347c2ac01a95f26b8ce15ca037dfac3204e
Radu Constantin <constantin.raduadrian@yahoo.com>
2019-08-21 14:26:11 +0300

    Created folder for variable exercises and deleted the old ones.

--- array_1.rb
+++
@@ -1,6 +0,0 @@
-arr = [1, 3, 5, 7, 9, 11]
-number = 3
-if arr.include? (number)
-  puts "This number is included in the array"
-else puts "This number is not in the array"
-end
--- name.rb
+++
@@ -1,8 +0,0 @@
-puts "What is your first name?"
-name = gets.chomp
-puts "What is your last name?"
-last_name = gets.chomp
-puts "Greetings #{name} #{last_name}!"
-10.times do |name|
-  puts name
-end
