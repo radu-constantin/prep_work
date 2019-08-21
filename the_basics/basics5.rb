2e894aa1506e1b0bacda439cc31f2667c3e58c77
Radu Constantin <constantin.raduadrian@yahoo.com>
2019-08-21 14:20:52 +0300

    finished the basics

--- the_basics/basics5.rb
+++ the_basics/basics5.rb
@@ -1,3 +1,9 @@
-n = 5
-def factorial(num)
-  n*(n-1)
+def factorial (num)
+  if num == 0 || num == 1
+    1
+  else
+    num * factorial(num-1)
+  end
+end
+
+puts factorial (4)
---
+++ the_basics/basics6.rb
@@ -0,0 +1,4 @@
+puts "What number do you want to find the square for?"
+number = gets.to_f
+square = number * number
+puts "The square of #{number} is #{square}."
